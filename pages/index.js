371c12c96a1302d738d1676266c7f95b5dc7dcef
Ryan Kray <rrich.kray.93@gmail.com>
2024-12-19 12:27:27 -0500

    whatever

--- pages/index.js
+++ pages/index.js
@@ -76,7 +76,6 @@ export default function Home() {
       }), 
     })
       .then((response) => {
-        console.log(response);
         window.location.replace("/");
       })
       .catch((err) => {
